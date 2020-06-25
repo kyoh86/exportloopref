6c1eb7a29e244927ace88c90892791fffbee7f47
Kyoichiro Yamada <me@kyoh86.dev>
2020-06-25 23:27:05 +0900

    counter test

--- exportloopref_test.go
+++ exportloopref_test.go
@@ -11,18 +11,27 @@ func TestSimple(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, exportloopref.Analyzer, "simple")
 }
+
 func TestStruct(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, exportloopref.Analyzer, "struct")
 }
+
 func TestComplex(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, exportloopref.Analyzer, "complex")
 }
+
 func TestFixed(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, exportloopref.Analyzer, "fixed")
 }
+
+func TestPslice(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, exportloopref.Analyzer, "pslice")
+}
+
 func TestIssue2(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, exportloopref.Analyzer, "issue2")
