bf0d0ab67d14e3f5d3149c747a07b962cbf2424a
Kyoichiro Yamada <me@kyoh86.dev>
2021-08-01 10:18:08 +0900

    fill test

--- testdata/src/break/break.go
+++ testdata/src/break/break.go
@@ -2,9 +2,23 @@ package main
 
 func main() {
 	var result *int
-	haystack := []int{1, 2}
-	for _, hay := range haystack {
-		result = &hay // Broken just below, the loop does NOT set unexpected value to result
+	list := []int{1, 2}
+	for _, p := range list {
+		result = &p // Broken just below, the loop does NOT set unexpected value to result
 		break
+	}
+
+	for _, p := range list {
+		result = &p // want "exporting a pointer for the loop variable p"
+		if *result == 1 {
+			break // Static analyzer can't judge the loop breaks always.
+		}
+	}
+
+	for _, p := range list {
+		result = &p // want "exporting a pointer for the loop variable p"
+		for {
+			break // it does not break `for _, p := range list`
+		}
 	}
 }
